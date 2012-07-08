dfaa3369f4606f2510d72ee435f2c817c30cdbf1
Alex Collins <alex.e.c@gmail.com>
2012-07-08 13:22:46 +0100

    Added demo for overriding plaf

---
+++ src/main/java/com/alexecollins/swing/plaf/overide/OverridingLookAndFeelDemoApp.java
@@ -0,0 +1,37 @@
+package com.alexecollins.swing.plaf.overide;
+
+import com.alexecollins.swing.plaf.synth.template.DemoFrame;
+
+import javax.swing.*;
+import javax.swing.plaf.synth.SynthLookAndFeel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Display demo components for the PLAF.
+ *
+ * @author alexec (alex.e.c@gmail.com)
+ */
+public class OverridingLookAndFeelDemoApp {
+    public static void main(String[] args) throws Exception {
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(new OverridingLookAndFeel());
+                    final DemoFrame f = new DemoFrame();
+                    f.addWindowListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosing(WindowEvent e) {
+                            System.exit(0);
+                        }
+                    });
+                    f.setVisible(true);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+    }
+}
